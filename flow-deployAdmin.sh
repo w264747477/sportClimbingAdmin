dd2f3dcc3b4b3a679bbfaed4f028006b88276627
wangqiqi <915281792@qq.com>
2023-06-22 17:07:50 +0800

    feat:服务器更换

--- flow-deployAdmin.sh
+++ flow-deployAdmin.sh
@@ -1,12 +1,12 @@
 echo -e "---------docker Login--------"
-docker login --username=$1 ccr.ccs.tencentyun.com --password=$2
+docker login --username=$1 registry.cn-beijing.aliyuncs.com --password=$2
 echo -e "---------docker Stop--------"
-docker stop scrapbookCt
+docker stop scrapadminbookCt
 echo -e "---------docker Rm--------"
-docker rm scrapbookCt
-docker rmi ccr.ccs.tencentyun.com/wqqahl/scrapbook:latest
+docker rm scrapadminbookCt
+docker rmi registry.cn-beijing.aliyuncs.com/qqzone/sportclimbingadmin:latest
 echo -e "---------docker Pull--------"
-docker pull ccr.ccs.tencentyun.com/wqqahl/scrapbook:latest
+docker pull registry.cn-beijing.aliyuncs.com/qqzone/sportclimbingadmin:latest
 echo -e "---------docker Create and Start--------"
-docker run --rm -d -p 8081:8081 --name scrapbookCt ccr.ccs.tencentyun.com/wqqahl/scrapbook:latest
+docker run --rm -d -p 8081:8081 --name scrapadminbookCt registry.cn-beijing.aliyuncs.com/qqzone/sportclimbingadmin:latest
 echo -e "---------deploy Success--------"
